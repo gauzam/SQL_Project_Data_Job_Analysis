dd90015117d658a9236c6c1c4c4fb7bb9a4f4353
gauzam <gautam01428@gmail.com>
2024-04-16 00:42:54 +0530

    updated query

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -34,12 +34,13 @@ JOIN
     job_postings_fact ON skills_job_dim.job_id = job_postings_fact.job_id
 WHERE
     job_title_short = 'Data Analyst' AND
+    job_work_from_home = TRUE AND
     salary_year_avg IS NOT NULL
 GROUP BY
     skills_dim.skill_id
 ORDER BY
     avg_salary DESC
-LIMIT 20;
+LIMIT 25;
 
 /*
 Here are some insights into the trends for the top paying skills for data analysts based on the provided data:
